92c855857d16ba141b4d4dec458e45b952de6692
keroppi <65063305+yejiseo0725@users.noreply.github.com>
2025-04-06 21:29:11 +0900

    js_반응형 웹에 맞추어 리사이징 되는 비디오 인터랙션

--- js/main.js
+++ js/main.js
@@ -134,7 +134,9 @@
       }
     }
     document.body.setAttribute("id", `show-scene-${currentScene}`);
-    // console.log(sceneInfo);
+
+    const heightRatio = window.innerHeight / 1080;
+    sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
   }
 
   function calcValues(values, currentScrollY) {
